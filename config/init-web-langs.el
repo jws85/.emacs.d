9ddcd8330c8251ebd6d1aaa6ddc5f01c140c663e
J. W. Smith <jwsmith2spam@gmail.com>
2014-10-17 20:56:14 -0400

    Enable auto-complete in PHP web-mode

--- config/init-web-langs.el
+++ config/init-web-langs.el
@@ -2,7 +2,17 @@
 
 (use-package web-mode
   :mode (("\\.html?\\'" . web-mode)
-	 ("\\.php\\'" . web-mode)))
+	 ("\\.php\\'" . web-mode))
+  :config
+  (progn
+    (add-to-list 'ac-modes 'web-mode)
+
+    (setq web-mode-ac-sources-alist
+	  '(("css" . (ac-source-words-in-buffer ac-source-css-property))
+	    ("html" . (ac-source-words-in-buffer ac-source-abbrev))
+	    ("php" . (ac-source-words-in-buffer
+		      ac-source-words-in-same-mode-buffers
+		      ac-source-dictionary))))))
 
 (add-hook 'css-mode-hook 'jws/hexcolor-add-to-font-lock)
 
