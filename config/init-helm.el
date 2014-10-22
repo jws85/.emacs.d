1ff99ee3ac1ac54509e0fef4525d09e535c1737a
J. W. Smith <jwsmith2spam@gmail.com>
2014-10-22 14:11:37 -0400

    Added basic config for helm
    I'm still not 'sold' on it but I'm a lot more comfortable with it

--- config/init-helm.el
+++ config/init-helm.el
@@ -3,12 +3,21 @@
 
 (use-package helm
   :ensure t
+  :init (require 'helm-config)
   :config
   (progn
+    (helm-mode 1)
+
+    ;; swaps C-z and <tab> -- this makes helm-find-files INFINITELY more usable
+    ;; cheers to http://tuhdo.github.io/helm-intro.html for this idea
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+    (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; terminal <tab>
+    (define-key helm-map (kbd "C-z") 'helm-select-action)
+
     (use-package helm-dash
       :ensure t
       :config
       (progn
-	(setq helm-dash-common-docsets '("C" "JavaScript" "jQuery" "MySQL" "PHP"))))))
+	(setq helm-dash-common-docsets '("Emacs Lisp" "C" "JavaScript" "jQuery" "MySQL" "PHP"))))))
 
 (provide 'init-helm)
