bdc655d9f9cb4d036cab6f1370eeccc6c2c8417b
J. W. Smith <jwsmith2spam@gmail.com>
2014-11-11 17:47:10 -0500

    Changed from auto-complete mode to company mode

--- config/init-auto-complete.el
+++ config/init-auto-complete.el
@@ -1,21 +1,21 @@
-(use-package auto-complete
+;; originally named auto-complete because I used auto-complete mode;
+;; but now I use company
+
+(use-package company
   :ensure t
-  :init
-  (progn
-    (require 'auto-complete-config)
-    (ac-config-default))
+  :init (company-mode)
   :config
   (progn
-    (after 'diminish
-      (diminish 'auto-complete-mode))
+    (define-key company-active-map (kbd "M-n") nil)
+    (define-key company-active-map (kbd "M-p") nil)
+    (define-key company-active-map (kbd "C-n") #'company-select-next-or-abort)
+    (define-key company-active-map (kbd "C-p") #'company-select-previous-or-abort)
 
-    (setq ac-comphist-file (concat user-emacs-directory ".cache/ac-comphist.dat"))
-    (setq ac-delay 0.05) ; completion list delay
-    (setq ac-auto-show-menu t)
-    (setq ac-quick-help-delay 0.3) ; tooltip delay
-    (setq ac-quick-help-height 30)
-    (setq ac-show-menu-immediately-on-auto-complete t)
+    (setq company-idle-delay 0.1
+	  company-minimum-prefix-length 1
+	  company-tooltip-limit 20
+	  company-selection-wrap-around t)
 
-    (after 'linum (ac-linum-workaround))))
+    (add-hook 'after-init-hook 'global-company-mode)))
 
 (provide 'init-auto-complete)
--- config/init-web-langs.el
+++ config/init-web-langs.el
@@ -2,16 +2,7 @@
   :ensure t
   :mode (("\\.html?\\'" . web-mode)
 	 ("\\.php\\'" . web-mode))
-  :config
-  (progn
-    (add-to-list 'ac-modes 'web-mode)
-
-    (setq web-mode-ac-sources-alist
-	  '(("css" . (ac-source-words-in-buffer ac-source-css-property))
-	    ("html" . (ac-source-words-in-buffer ac-source-abbrev))
-	    ("php" . (ac-source-words-in-buffer
-		      ac-source-words-in-same-mode-buffers
-		      ac-source-dictionary))))))
+  :init (require 'web-mode))
 
 (add-hook 'css-mode-hook 'jws/hexcolor-add-to-font-lock)
 
