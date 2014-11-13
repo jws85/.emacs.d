fbc2b2fe3a158e513b3497f00b790a6fa19f2bab
J. W. Smith <jwsmith2spam@gmail.com>
2014-11-13 12:50:04 -0500

    Removed unused keybindings from eww

--- config/init-eww.el
+++ config/init-eww.el
@@ -5,20 +5,17 @@
 (eval-after-load "eww"
   '(progn
      ;; Use vim keybindings for searching
-     (define-key eww-mode-map (read-kbd-macro "/") 'evil-search-forward)
-     (define-key eww-mode-map (read-kbd-macro "?") 'evil-search-backward)
-     (define-key eww-mode-map (read-kbd-macro "n") 'evil-search-next)
-     (define-key eww-mode-map (read-kbd-macro "N") 'evil-search-previous)
+     (define-key eww-mode-map (kbd "/") 'evil-search-forward)
+     (define-key eww-mode-map (kbd "?") 'evil-search-backward)
+     (define-key eww-mode-map (kbd "n") 'evil-search-next)
+     (define-key eww-mode-map (kbd "N") 'evil-search-previous)
+
      ;; Use vim keybindings for scrolling
-     (define-key eww-mode-map (read-kbd-macro "j") 'evil-next-line)
-     (define-key eww-mode-map (read-kbd-macro "k") 'evil-previous-line)
-     (define-key eww-mode-map (read-kbd-macro "C-j") (lambda () (interactive) (next-line 2) (scroll-up 2)))
-     (define-key eww-mode-map (read-kbd-macro "C-k") (lambda () (interactive) (scroll-down 2) (previous-line 2)))
-     (define-key eww-mode-map (read-kbd-macro "d") 'evil-scroll-down)
-     (define-key eww-mode-map (read-kbd-macro "u") 'evil-scroll-up)
+     (define-key eww-mode-map (kbd "j") 'evil-next-line)
+     (define-key eww-mode-map (kbd "k") 'evil-previous-line)
+
      ;; Use sane keybindings for forward/back
-     (define-key eww-mode-map (read-kbd-macro "b") 'eww-back-url)
-     (define-key eww-mode-map (read-kbd-macro "<backspace>") 'eww-back-url)
-     (define-key eww-mode-map (read-kbd-macro "S-<backspace>") 'eww-forward-url)))
+     (define-key eww-mode-map (kbd "<backspace>") 'eww-back-url)
+     (define-key eww-mode-map (kbd "C-<backspace>") 'eww-forward-url)))
 
 (provide 'init-eww)
