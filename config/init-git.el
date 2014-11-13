db4756331757599fe1b5df1b9053b1de4fa3932c
J. W. Smith <jwsmith2spam@gmail.com>
2014-11-13 18:08:55 -0500

    hjkl keybindings in magit

--- config/init-git.el
+++ config/init-git.el
@@ -3,7 +3,14 @@
   :config
   (progn
     (after 'evil
+      (evil-add-hjkl-bindings magit-status-mode-map 'emacs
+	"l" 'magit-key-mode-popup-logging)
+      (evil-add-hjkl-bindings magit-log-mode-map 'emacs)
+      (evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
+      (evil-add-hjkl-bindings magit-diff-mode-map 'emacs)
+
       (define-key evil-normal-state-map (kbd ", g") 'magit-status))
+
     (after 'diminish
       (diminish 'magit-auto-revert-mode))))
 
