ffd2a8fb5e0ee878a29827f2d7b799dcc70ae258
J. W. Smith <jwsmith2spam@gmail.com>
2014-11-13 15:31:28 -0500

    Enable paredit for Lisp modes

--- config/init-lisp.el
+++ config/init-lisp.el
@@ -8,7 +8,15 @@
   :ensure t
   :config
   (after 'evil
+    (after 'diminish
+      (diminish 'paredit-mode))
+
     (use-package evil-paredit
-      :ensure t)))
+      :ensure t)
+
+    (add-hook 'emacs-lisp-mode #'enable-paredit-mode)
+    (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+    (add-hook 'scheme-mode-hook #'enable-paredit-mode)))
 
 (provide 'init-lisp)
