227ea77ce6a933c0543bf6f9c0147b75503e4495
J. W. Smith <jwsmith2spam@gmail.com>
2015-04-29 18:04:13 -0400

    Added a visual indication of progress in file
    Nyan~

--- lisp/config/modeline.el
+++ lisp/config/modeline.el
@@ -1,14 +1,17 @@
 ;; Not quite as sexy as airline or powerline, but neither worked particularly
 ;; well in Windoze anyways (too much Unicode faffery) so here we are.
-;;
-;; I need to rein in my tendency to make my emacs (ugh) 'lickable' as it were.
-;; It's out of the 70s anyways, it should look like it!
 (use-package smart-mode-line
   :ensure t
   :config
   (progn
+    ;; Hide packages from the modeline
     (use-package diminish
       :ensure t)
+
+    ;; Nyan~ ^_^ (in all seriousness, it's not a bad navigation indicator)
+    (use-package nyan-mode
+      :ensure t
+      :init (nyan-mode))
 
     (column-number-mode)
 
