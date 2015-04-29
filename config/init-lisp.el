2ddfbfa4234bf9b847d5521d7e0398c277f6830c
J. W. Smith <jwsmith2spam@gmail.com>
2015-04-29 11:28:37 -0400

    I couldn't get used to paredit-mode
    Just too radical of a change.  I like being able to copy lines and
    such without being disallowed because of a parens mismatch.

--- config/init-lisp.el
+++ config/init-lisp.el
@@ -4,27 +4,4 @@
 (use-package slime
   :ensure t)
 
-(use-package paredit
-  :ensure t
-  :config
-  (after 'evil
-    (after 'diminish
-      (diminish 'paredit-mode))
-
-    (use-package evil-paredit
-      :ensure t
-      :config
-      (progn
-	;; otherwise the ordinary evil delete will happily eat your parens
-	(evil-define-key 'insert evil-paredit-mode-map (kbd "<backspace>") #'paredit-backward-delete)
-
-	(defun jws/paredit-hook ()
-	  (enable-paredit-mode)
-	  (evil-paredit-mode))
-
-	(add-hook 'emacs-lisp-mode-hook #'jws/paredit-hook)
-	(add-hook 'lisp-mode-hook #'jws/paredit-hook)
-	(add-hook 'lisp-interaction-mode-hook #'jws/paredit-hook)
-	(add-hook 'scheme-mode-hook #'jws/paredit-hook)))))
-
 (provide 'init-lisp)
