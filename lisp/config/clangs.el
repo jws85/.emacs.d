d11f9e18285bc01b9364e70433edc99e903609d2
J. W. Smith <jwsmith2spam@gmail.com>
2015-05-06 19:59:03 -0400

    Enable smart-tabs-mode for C-like languages

--- lisp/config/clangs.el
+++ lisp/config/clangs.el
@@ -3,6 +3,6 @@
 (add-hook 'c-mode-common-hook
           (lambda ()
             (modify-syntax-entry ?_ "w")  ; underscore considered part of a "word"
-            ))
+            (smart-tabs-insinuate 'c 'c++ 'java)))
 
 (provide 'config/clangs)
