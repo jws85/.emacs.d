d73ccf7b233ec6ad5f177b54238f85a194ff40ce
J. W. Smith <jwsmith2spam@gmail.com>
2015-05-06 19:22:48 -0400

    Add hyphen to Lisp word elements
    I'm going to be adding a lot of things like this, because I use
    Evil's * (search for word at point) command, and nothing frustrates
    me more than not getting the whole word like Vim does.

--- lisp/config/lisp.el
+++ lisp/config/lisp.el
@@ -1,6 +1,11 @@
 ;; Use Quicklisp to install swank in your preferred lisp and define
 ;; the lisp using slime-lisp-implementations
 
+(defun jws/lisp-hook ()
+  (modify-syntax-entry ?- "w"))
+(add-hook 'lisp-mode-hook #'jws/lisp-hook)
+(add-hook 'emacs-lisp-mode-hook #'jws/lisp-hook)
+
 (use-package slime
   :ensure t)
 
