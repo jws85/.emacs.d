1b84a596fd15f35052b97ffa1e594e4b3545cbe3
J. W. Smith <jwsmith2spam@gmail.com>
2015-05-27 18:39:40 -0400

    Disabled some IRRITATING web-mode defaults
    For developing new code, auto-quoting and auto-pairing are fantastic!
    Unfortunately, for maintenance programming, these two settings are
    probably contributing to premature hair loss.
    
    I'd like web-mode to expose a keybind to let me manually pair when
    I'm done with a tag or a PHP block or whatever.  (Maybe they already
    do!)

--- lisp/config/web-langs.el
+++ lisp/config/web-langs.el
@@ -2,7 +2,11 @@
   :ensure t
   :mode (("\\.html?\\'" . web-mode)
 	 ("\\.php\\'" . web-mode))
-  :init (require 'web-mode))
+  :init (require 'web-mode)
+  :config
+  (progn
+    (setq web-mode-enable-auto-pairing nil
+          web-mode-enable-auto-quoting nil)))
 
 (require 'jws/hexcolor)
 (add-hook 'css-mode-hook 'jws/hexcolor-add-to-font-lock)
