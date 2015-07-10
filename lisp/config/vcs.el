0921f62e0cc213481698b44c4796b34284dbb6e5
J. W. Smith <jwsmith2spam@gmail.com>
2015-07-10 16:13:49 -0400

    Well, let's move magit from j/k to C-n/C-p for now
    ...even though it feels like you're going from a $80k BMW sedan to the
    local pizza boy's 1988 Honda Civic.
    
    I imagine if I disable everything else, I'll remember not to push
    'k' which could potentially cause data loss.

--- lisp/config/vcs.el
+++ lisp/config/vcs.el
@@ -5,12 +5,13 @@
     (setq magit-last-seen-setup-instructions "1.4.0")
     
     (after 'evil
-      (evil-add-hjkl-bindings magit-status-mode-map 'emacs
-        "l" 'magit-key-mode-popup-logging
-        "v" 'set-mark-command)
-      (evil-add-hjkl-bindings magit-log-mode-map 'emacs)
-      (evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
-      (evil-add-hjkl-bindings magit-diff-mode-map 'emacs)
+      ;; FIXME these binds don't work, just use C-n/C-p for now
+      ;(evil-add-hjkl-bindings magit-status-mode-map 'emacs
+      ;  "l" 'magit-key-mode-popup-logging
+      ;  "v" 'set-mark-command)
+      ;(evil-add-hjkl-bindings magit-log-mode-map 'emacs)
+      ;(evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
+      ;(evil-add-hjkl-bindings magit-diff-mode-map 'emacs)
 
       (define-key evil-normal-state-map (kbd ", g") 'magit-status))))
 
