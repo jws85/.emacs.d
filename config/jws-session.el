7ee39ffcb90321920884013a5fbb302cfaad9c65
Justin Smith <jsmith@inttekmail.inttek.net>
2017-05-10 22:57:15 -0400

    Fix annoying desktop-mode code for abnormal exit
    If emacs is killed abnormally (which does happen to me; I tend to run
    bleeding-edge releases), I don't want to have to come back a day or
    more later and find that I need to answer desktop-save-mode's annoying
    bootup message.  I want to start coding!  So I put this hack in, which
    I found from emacswiki as indicated in the file.

--- config/jws-session.el
+++ config/jws-session.el
@@ -1,5 +1,27 @@
 ;; Session management
 
+;;;; The following code was stolen from https://www.emacswiki.org/emacs/Desktop#toc4
+;;;; What it does is:  whenever emacs ends abnormally (it crashes, the system crashes,
+;;;; or when I shut down without shutting Emacs down), I get stuck with a stale lock
+;;;; on the desktop file, which is irritating.
+;;;;
+;;;; So this should attempt to remove the dead lock file and load anyways.
+(defun emacs-process-p (pid)
+  "If pid is the process ID of an emacs process, return t, else nil.
+Also returns nil if pid is nil."
+  (when pid
+    (let ((attributes (process-attributes pid)) (cmd))
+      (dolist (attr attributes)
+        (if (string= "comm" (car attr))
+            (setq cmd (cdr attr))))
+      (if (and cmd (or (string= "emacs" cmd) (string= "emacs.exe" cmd))) t))))
+
+(defadvice desktop-owner (after pry-from-cold-dead-hands activate)
+  "Don't allow dead emacsen to own the desktop file."
+  (when (not (emacs-process-p ad-return-value))
+    (setq ad-return-value nil)))
+;;;; end emacswiki code
+
 ;; Create directory
 (if (not (file-exists-p jws/emacs-desktop-dir))
     (make-directory jws/emacs-desktop-dir t))
