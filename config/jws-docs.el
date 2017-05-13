1e3f7326908a21d3b3acfa90973cf0d13f5b73b2
Justin Smith <jsmith@inttekmail.inttek.net>
2017-05-12 22:35:47 -0400

    Added zeal-at-point
    Yeah it doesn't have the gee-whiz appeal of firing up eww within emacs
    to display help, but something like zeal Just Works(TM), dangit
    
    I may have to consider a binding other than C-c d.

--- config/jws-docs.el
+++ config/jws-docs.el
@@ -3,4 +3,10 @@
       (cons (expand-file-name (concat user-emacs-directory "info"))
             Info-default-directory-list))
 
+(use-package zeal-at-point
+  :ensure t
+  :config
+  (progn
+    (global-set-key "\C-cd" 'zeal-at-point)))
+
 (provide 'jws-docs)
