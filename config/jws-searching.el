4213d598652d3497c49f00ceb928a92c76018c95
Justin Smith <jsmith@inttekmail.inttek.net>
2017-06-16 20:22:09 -0400

    ivy is an almost perfect merging of ido and helm
    with a heck of a lot less hairy configuration than either.
    
    A lot of the below is very subjective; I think helm is a neat
    package too, and ivy owes a lot to it.
    
    ido:
      + comes with emacs
      + fast, both in performance and in keystrokes
      + with flx, pretty nice completion
      - needs a ton of other people's packages to do much
      - interface tries to not be super intrusive, which means it is
        limited in what it can do sometimes... without more package
    
    helm:
      + does everything and the kitchen sink
      + tries to be a unified interface to finding things
      + did i mention that it can do everything?
      - does not come with emacs
      - feels heavy
      - even after significant refactoring and TLC, it still needs a
        lot of work and research to configure
      - keybindings not the most intuitive
      - completion method a little alien to those who like to
        tab-complete
    
    ivy:
      + sort of like a "unifying interface" like helm
      + but works more like ido
      + feels faster than helm
      + much less configuration needed than either
      + feels "more modern" than helm
      - does not come with emacs
      - some keybindings still a little wonky (when I press RET on a
        directory, I don't want dired, I want to go into the directory)
    
    I get that helm/ivy want to be this unifying interface, but I really
    wish they'd break their rules with *-find-files and make RET mean
    "open the file if it is a file, otherwise open helm/ivy on this
    directory" and not "open the file, and if it is a directory, open it
    in dired"

--- config/jws-searching.el
+++ config/jws-searching.el
@@ -1,104 +1,42 @@
-(require 'ido)
+;; Lossage in switch to ivy:
+;;  ,eo -- #'helm-occur, which is basically swiper
+;;  ,ek -- #'helm-show-kill-ring
+;;  ,ef -- #'helm-semantic-or-imenu
+;;  ,er -- #'helm-regexp
+;;  ,ec -- #'helm-colors
+;;  ,ew -- #'helm-surfraw
 
-;; enables ido-mode in as many places as possible...
-(ido-mode t)
-(ido-everywhere t)
-
-;; including everywhere where there's completing-read
-(use-package ido-ubiquitous
-  :ensure t
-  :init (ido-ubiquitous-mode 1))
-
-(use-package ido-vertical-mode
+(use-package ivy
   :ensure t
   :init
   (progn
-    (ido-vertical-mode 1)
-    (setq ido-vertical-define-keys 'C-n-and-C-p-only)))
-
-;; ido is super-automated.  This is awesome in a lot of ways, but it often
-;; gets in the way of me actually doing work, e.g. creating new files.
-
-;; disable searching in unrelated directories; e.g. I'm in ~/a/b/c and
-;; decide to make a new file ~/a/b/c/foobar, but I was recently in ~/z
-;; which has ~/z/foobar; ido will present me with ~/z/foobar which is
-;; often infuriating
-(setq ido-auto-merge-work-directories-length -1)
-
-;; require pressing enter to confirm the current directory
-(setq ido-confirm-unique-completion nil)
-(setq ido-enable-tramp-completion nil)
-
-;; map find-file and switch-buffer to easier keys
-(after 'evil
-  (define-key evil-normal-state-map (kbd ", f") 'ido-find-file)
-  (define-key evil-normal-state-map (kbd ", b") 'ido-switch-buffer))
-
-(defun jws/ido-hook ()
-  (define-key ido-completion-map (kbd "<down>") 'ido-next-match)
-  (define-key ido-completion-map (kbd "<up>") 'ido-prev-match))
-
-(add-hook 'ido-setup-hook 'jws/ido-hook)
-
-;; enables a 'nicer' matching method (warning: possible performance killer,
-;; but on this old AMD here, and my i5-3570k back home, it works fine)
-(use-package flx-ido
-  :ensure t
-  :init (flx-ido-mode t))
-
-;; ido niceties in the M-x command interface
-(use-package smex
-  :ensure t
-  :init (smex-initialize)
+    (require 'ivy)
+    (ivy-mode 1))
   :config
   (progn
-    (global-set-key (kbd "M-x") 'smex)
+    (use-package counsel :ensure t :init (require 'counsel))
+    (use-package swiper :ensure t :init (require 'swiper))
+
+    (setq ivy-use-virtual-buffers t
+          ivy-count-format "(%d/%d)")
+
+    (global-set-key (kbd "C-s") 'swiper)
+
+    (after 'projectile
+      (setq projectile-completion-system 'ivy))
+
+    (after 'magit
+      (setq magit-completing-read-function 'ivy-completing-read))
+
     (after 'evil
-      (define-key evil-normal-state-map (kbd ";") 'smex)
-      (define-key evil-visual-state-map (kbd ";") 'smex))))
-
-;; I don't like helm-find-files, but I find a lot of the helm commands
-;; to be otherwise nifty.  This is why I just do helm-config and don't
-;; run the full helm-mode.
-(use-package helm
-  :ensure t
-  :init (require 'helm-config)
-  :config
-  (progn
-    (after 'evil
-      ; searchable character map
-      (define-key evil-normal-state-map (kbd ", e u") 'helm-ucs)
-
-      ; find string in buffer
-      (define-key evil-normal-state-map (kbd ", e o") 'helm-occur)
-
-      ; show clipboard
-      (define-key evil-normal-state-map (kbd ", e k") 'helm-show-kill-ring)
-
-      ; find function definitions
-      (define-key evil-normal-state-map (kbd ", e f") 'helm-semantic-or-imenu)
-
-      ; run an elisp regex against the buffer
-      (define-key evil-normal-state-map (kbd ", e r") 'helm-regexp)
-
-      ; find a file using 'locate' binary (uses 'everything' on Windows)
-      (define-key evil-normal-state-map (kbd ", e l") 'helm-locate)
-
-      ; searchable list of colors
-      (define-key evil-normal-state-map (kbd ", e c") 'helm-colors)
-
-      ; uses 'surfraw' to look up things online
-      (define-key evil-normal-state-map (kbd ", e w") 'helm-surfraw)
-
-      ; look up man pages
-      (define-key evil-normal-state-map (kbd ", e m") 'helm-man-woman))
-
-    ; use "The Silver Searcher" (ag) binary to find files in project
-    (use-package helm-ag
-      :ensure t
-      :config
-      (setq helm-ag-insert-at-point 'word)
-      (after 'evil
-        (define-key evil-normal-state-map (kbd ", e g") 'helm-ag-project-root)))))
+      (define-key evil-normal-state-map (kbd "/") 'swiper)
+      (define-key evil-normal-state-map (kbd ";") 'counsel-M-x)
+      (define-key evil-normal-state-map (kbd ":") 'counsel-M-x)
+      (define-key evil-normal-state-map (kbd ", f") 'counsel-find-file)
+      (define-key evil-normal-state-map (kbd ", b") 'ivy-switch-buffer)
+      (define-key evil-normal-state-map (kbd ", e u") 'counsel-unicode-char)
+      (define-key evil-normal-state-map (kbd ", e l") 'counsel-locate)
+      (define-key evil-normal-state-map (kbd ", e g") 'counsel-ag)
+      (define-key evil-normal-state-map (kbd ", e m") 'woman))))
 
 (provide 'jws-searching)
