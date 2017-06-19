4c198b648efc1c4e8b94e53b2c5d8d746df49785
Justin Smith <jsmith@inttekmail.inttek.net>
2017-06-19 12:04:55 -0400

    Better grepping within projects
    Use counsel-projectile-ag within projectile projects; and counsel-ag
    elsewhere.
    
    I'm hoping the double binding on ,pg isn't going to cause a problem...

--- config/jws-searching.el
+++ config/jws-searching.el
@@ -22,9 +22,15 @@
     (use-package swiper :ensure t :init (require 'swiper))
     (use-package flx :ensure t :init (require 'flx))
 
+    (after 'projectile
+      (use-package counsel-projectile :ensure t
+        :init (require 'counsel-projectile)
+        :config (counsel-projectile-on)))
+
     (setq ivy-use-virtual-buffers t
           ivy-count-format "(%d/%d)"
-          ivy-magic-tilde nil)
+          ivy-magic-tilde nil
+          counsel-find-file-at-point t)
 
     ;; I'm not sure of the implication of this... but it fixes an irritating
     ;; impedance mismatch between helm/ivy and ido.  When you use RET in
@@ -42,6 +48,17 @@
       (setq magit-completing-read-function 'ivy-completing-read))
 
     (after 'evil
+      (after 'projectile
+        (define-key evil-normal-state-map (kbd ", p g") 'counsel-projectile-ag)
+        ;; If projectile is active in a buffer, use the projectile project as the
+        ;; ag bounds; otherwise just ag in the file's directory
+        (defun jws/counsel-ag-contextual ()
+          (interactive)
+          (if (projectile-project-p)
+              (counsel-projectile-ag)
+            (counsel-ag)))
+        (define-key evil-normal-state-map (kbd ", e g") 'jws/counsel-ag-contextual))
+
       (define-key evil-normal-state-map (kbd "/") 'counsel-grep-or-swiper)
       (define-key evil-normal-state-map (kbd ";") 'counsel-M-x)
       (define-key evil-normal-state-map (kbd ":") 'counsel-M-x)
@@ -49,7 +66,6 @@
       (define-key evil-normal-state-map (kbd ", b") 'ivy-switch-buffer)
       (define-key evil-normal-state-map (kbd ", e u") 'counsel-unicode-char)
       (define-key evil-normal-state-map (kbd ", e l") 'counsel-locate)
-      (define-key evil-normal-state-map (kbd ", e g") 'counsel-ag)
       (define-key evil-normal-state-map (kbd ", e m") 'woman))))
 
 (provide 'jws-searching)
