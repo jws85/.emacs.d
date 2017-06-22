6f1a46a4042a3e112d6fbeca004849e7dd1b08ec
Justin Smith <jsmith@inttekmail.inttek.net>
2017-06-22 11:16:57 -0400

    Command to show Emacs clipboard stuff

--- config/jws-searching.el
+++ config/jws-searching.el
@@ -1,6 +1,5 @@
 ;; Lossage in switch to ivy:
 ;;  ,eo -- #'helm-occur, which is basically swiper
-;;  ,ek -- #'helm-show-kill-ring
 ;;  ,ef -- #'helm-semantic-or-imenu
 ;;  ,er -- #'helm-regexp
 ;;  ,ew -- #'helm-surfraw
@@ -66,6 +65,7 @@
       (define-key evil-normal-state-map (kbd ", e u") 'counsel-unicode-char)
       (define-key evil-normal-state-map (kbd ", e l") 'counsel-locate)
       (define-key evil-normal-state-map (kbd ", e c") 'counsel-colors-web)
+      (define-key evil-normal-state-map (kbd ", e k") 'counsel-yank-pop)
       (define-key evil-normal-state-map (kbd ", e m") 'woman))))
 
 (provide 'jws-searching)
