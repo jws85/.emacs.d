f8a106d65e7cab4cb487aac8c8a3786145158d7a
J. W. Smith <jwsmith2spam at gmail dot com>
2017-06-24 11:33:35 -0400

    Hacky usage of spaceline-all-the-icons
    I like spaceline-all-the-icons but you need to install icon fonts for
    it to look right, and at the moment I'm not aware of a way to check
    for the existence of these fonts.
    
    And you lose nyan-mode T_T  If I ever get a wild hair, I might see what
    needs to be done to get nyan-mode to work.
    
    Right now, I just have the old smart-mode-line as default, and then
    the user can require 'jws-modeline-fancy if they want.

--- config/jws-modeline.el
+++ config/jws-modeline.el
@@ -1,51 +1,36 @@
+; Not quite as sexy as airline or powerline, but neither worked particularly
+; well in Windoze anyways (too much Unicode faffery) so here we are.
+(use-package smart-mode-line
+  :ensure t
+  :config
+  (progn
+    ;; Hide packages from the modeline
+    (use-package diminish
+      :ensure t)
 
-(use-package diminish
-  :ensure t)
+    ;; Nyan~ ^_^ (in all seriousness, it's not a bad navigation indicator)
+    (use-package nyan-mode
+      :ensure t
+      :init (nyan-mode)
+      :config
+      (progn
+        (setq nyan-bar-length 25)))
 
-(require 'spaceline-config)
-(require 'nyan-mode)
-(nyan-mode)
-(setq nyan-bar-length 20)
-(setq spaceline-buffer-size-p nil
-      spaceline-evil-state-p t
-      spaceline-minor-modes-p nil
-      spaceline-nyan-cat-p t
-      spaceline-highlight-face-func #'spaceline-highlight-face-evil-state)
-(spaceline-emacs-theme)
+    (column-number-mode)
 
-;; Not quite as sexy as airline or powerline, but neither worked particularly
-;; well in Windoze anyways (too much Unicode faffery) so here we are.
-;(use-package smart-mode-line
-;  :ensure t
-;  :config
-;  (progn
-;    ;; Hide packages from the modeline
-;    (use-package diminish
-;      :ensure t)
-;
-;    ;; Nyan~ ^_^ (in all seriousness, it's not a bad navigation indicator)
-;    (use-package nyan-mode
-;      :ensure t
-;      :init (nyan-mode)
-;      :config
-;      (progn
-;        (setq nyan-bar-length 25)))
-;
-;    (column-number-mode)
-;
-;    (if after-init-time (sml/setup)
-;      (add-hook 'after-init-hook 'sml/setup))
-;
-;    ;; three places for column numbers
-;    (setq sml/col-number-format "%3c")
-;
-;    ;; hide buffer encoding type -- I just don't need it that much
-;    (setq sml/mule-info "")
-;
-;    ;; make filename field less wide
-;    (setq sml/name-width 38)
-;
-;    ;; right-align the list of minor-modes
-;    (setq sml/mode-width 'right)))
+    (if after-init-time (sml/setup)
+      (add-hook 'after-init-hook 'sml/setup))
+
+    ;; three places for column numbers
+    (setq sml/col-number-format "%3c")
+
+    ;; hide buffer encoding type -- I just don't need it that much
+    (setq sml/mule-info "")
+
+    ;; make filename field less wide
+    (setq sml/name-width 38)
+
+    ;; right-align the list of minor-modes
+    (setq sml/mode-width 'right)))
 
 (provide 'jws-modeline)
