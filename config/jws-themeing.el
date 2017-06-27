6e9fca233875a26668f3201e8ac68f85a75e413a
Justin Smith <jsmith@inttekmail.inttek.net>
2017-06-27 13:58:07 -0400

    Pull in a couple themes that I like
    Be warned that I'm not installing them through use-package because
    use-package loads all the theme packages, thus barfing colors from all
    the other packages over whatever theme you eventually load.

--- config/jws-themeing.el
+++ config/jws-themeing.el
@@ -27,4 +27,32 @@
   (after 'evil
     (define-key evil-normal-state-map (kbd ", t") 'jws/hydra-themeing/body)))
 
+;; Color themes
+
+;; Below I install a few themes I liked.
+
+;; I include a port of the Vim "Jellybeans" theme in the ~/.emacs.d/colors/ folder;
+;; it can be simply loaded with load-theme.  I should put this in MELPA.
+
+;; Be forewarned that many themes will often define library faces that are different
+;; than other themes will, so the end result is that you may have some ugly colors
+;; from a previous theme clashing with the currently selected one...
+
+;; FIXME use-package has no facilities for installing a package but never loading
+;; it.  This is problematic for installing themes, which often load faces when their
+;; library is loaded...
+
+;; Theme families
+;; These may come with additional packages to provide various features; go to
+;; the theme pages to learn more.
+(package-install 'moe-theme)
+(package-install 'solarized-theme)
+
+;; Dark themes
+(package-install 'cyberpunk-theme)
+(package-install 'zenburn-theme)
+
+;; Light themes
+(package-install 'leuven-theme)
+
 (provide 'jws-themeing)
