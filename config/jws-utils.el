45613b4a461a6eb2c0e423f2409939beb1c14c56
Justin Smith <jsmith@inttekmail.inttek.net>
2017-07-05 17:20:24 -0400

    Added make-it-so
    This is a batch file converter for Emacs Dired, that works in a pretty
    interesting way.  Bit tricky to learn at first though!  Hope you know
    how to use makefiles!

--- config/jws-utils.el
+++ config/jws-utils.el
@@ -1,5 +1,14 @@
+;; Turn off evil-mode when viewing images
 (require 'image-mode)
-
 (evil-set-initial-state 'image-mode 'emacs)
+
+;; make-it-so, a package to perform batch file conversions with dired
+(use-package make-it-so
+  :ensure t
+  :init (require 'make-it-so)
+  :config
+  (progn
+    (setq mis-recipes-directory (expand-file-name (concat user-emacs-directory "etc/make-it-so")))
+    (define-key dired-mode-map (kbd ", ,") 'make-it-so)))
 
 (provide 'jws-utils)
