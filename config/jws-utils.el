453636fa9ef4cc12e4a2811ea4e3e6ba174694cc
J. W. Smith <jwsmith2spam at gmail dot com>
2017-09-10 19:51:05 -0400

    Added some code to handle the emacs shell
    Hopefully I can make it non-clunky, it would be nice to run shells
    inside of emacs

--- config/jws-utils.el
+++ config/jws-utils.el
@@ -1,3 +1,13 @@
+;; Disable company-mode in shell, to prevent needing to hit RET twice
+(defun jws/disable-completion-in-shell ()
+  (company-mode -1)
+  (setq-local company-mode nil))
+(add-hook 'shell-mode-hook #'jws/disable-completion-in-shell)
+
+;; Open up shell through shortcut
+(global-set-key (kbd "M-j s") 'shell)
+(after 'evil (define-key evil-normal-state-map (kbd ", c") 'shell))
+
 ;; Turn off evil-mode when viewing images
 (require 'image-mode)
 (evil-set-initial-state 'image-mode 'emacs)
