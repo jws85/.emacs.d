75e8ed9ff1d96beb91e4c3f187619792e477f29d
Justin Smith <jsmith@inttekmail.inttek.net>
2017-09-11 13:51:12 -0400

    Added midnight-mode... though I may never use it
    At least on my work box, which gets logged out every night.
    
    Daemonizing emacs will help here... I need to figure out how to make
    that happen.

--- config/jws-session.el
+++ config/jws-session.el
@@ -37,4 +37,11 @@ Also returns nil if pid is nil."
 ;; Always save the session
 (desktop-save-mode 1)
 
+;; This should go through my buffers once a day, early in the morning,
+;; and get rid of a lot of cruft (any buffer older than three days
+;; old, plus temp buffer junk) BTW, if I'm up at 3:30 AM, something is
+;; seriously wrong.
+(require 'midnight)
+(midnight-delay-set 'midnight-delay "3:30 AM")
+
 (provide 'jws-session)
