9fae94882282138a11d7a5a8a3de607e713154de
Justin Smith <jsmith@inttekmail.inttek.net>
2017-12-29 14:05:03 -0500

    Set items to display on the splashscreen

--- config/jws-splashscreen.el
+++ config/jws-splashscreen.el
@@ -1,6 +1,9 @@
 (use-package dashboard
   :ensure t
   :config
-  (dashboard-setup-startup-hook))
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((agenda . 5)
+                          (projects . 5)
+                          (recents . 5))))
 
 (provide 'jws-splashscreen)
