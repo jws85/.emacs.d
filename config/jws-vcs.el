0b5feac5c2b32d20c56fef200276183c727dcba5
Justin Smith <jsmith@inttekmail.inttek.net>
2017-12-29 23:26:15 -0500

    Clean up more awful progn stuff
    ...originally this was going to add magit-gitflow, but I can't get it
    to work.  So I'm just going to add the progn cleanup in that file.

--- config/jws-vcs.el
+++ config/jws-vcs.el
@@ -1,21 +1,19 @@
 (use-package magit
   :ensure t
   :config
-  (progn
-    (setq magit-last-seen-setup-instructions "1.4.0")
+  (setq magit-last-seen-setup-instructions "1.4.0")
 
-    (after 'diminish
-      (diminish 'auto-revert-mode))
+  (after 'diminish
+    (diminish 'auto-revert-mode))
 
-    (global-set-key (kbd "M-j g") 'magit-status)
+  (global-set-key (kbd "M-j g") 'magit-status)
 
-    (after 'evil
-      (use-package evil-magit
-        :ensure t
-        :config
-        (progn
-          (require 'evil-magit)))
+  (after 'evil
+    (use-package evil-magit
+      :ensure t
+      :config
+      (require 'evil-magit))
 
-      (define-key evil-normal-state-map (kbd "SPC g") 'magit-status))))
+    (define-key evil-normal-state-map (kbd "SPC g") 'magit-status)))
 
 (provide 'jws-vcs)
