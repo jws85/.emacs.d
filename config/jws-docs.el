63860698318bd0b3b44439981f6b14f5fb32ac69
Justin Smith <jsmith@inttekmail.inttek.net>
2018-03-09 20:50:12 -0500

    Added "help" keybindings

--- config/jws-docs.el
+++ config/jws-docs.el
@@ -3,11 +3,24 @@
       (cons (expand-file-name (concat user-emacs-directory "info"))
             Info-default-directory-list))
 
+(global-set-key (kbd "M-j h i") 'info)
+(define-key evil-normal-state-map (kbd "SPC h i") 'info)
+
+(global-set-key (kbd "M-j h f") 'describe-function)
+(define-key evil-normal-state-map (kbd "SPC h f") 'describe-function)
+
+(global-set-key (kbd "M-j h v") 'describe-variable)
+(define-key evil-normal-state-map (kbd "SPC h v") 'describe-variable)
+
+;; [FIXME] would like to replace with counsel version of same... that doesn't exist...
+(global-set-key (kbd "M-j h m") 'helm-man-woman)
+(define-key evil-normal-state-map (kbd "SPC h m") 'helm-man-woman)
+
 (use-package zeal-at-point
   :ensure t
   :config
   (progn
-    (define-key evil-normal-state-map (kbd "SPC d") 'zeal-at-point)
-    (global-set-key "\C-hz" 'zeal-at-point)))
+    (global-set-key (kbd "M-j h d") 'zeal-at-point)
+    (define-key evil-normal-state-map (kbd "SPC h d") 'zeal-at-point)))
 
 (provide 'jws-docs)
