6fa7d6669ba73c77b49be2a32ac6151e5e8a67f3
Justin Smith <jsmith@inttekmail.inttek.net>
2018-04-06 19:51:31 -0400

    I forgot why this is there
    welp

--- init.el
+++ init.el
@@ -18,6 +18,12 @@
 
 ;;;; Global variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defvar jws/emacs-backup-dir (expand-file-name (concat user-emacs-directory ".cache/backups"))
   "The directory that emacs uses to store backup files to")
 
