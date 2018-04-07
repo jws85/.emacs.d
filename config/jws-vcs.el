a19a9ff86c2caba1c3db6d7b36bab3198c8d7c1e
Justin Smith <jsmith@inttekmail.inttek.net>
2018-04-07 00:21:52 -0400

    Added magit-gitflow on % keybinding
    The default is C-f, which evil-magit uses for pagedown... and I found
    it "impossible" to remove that binding!
    
    Spacemacs has magit-gitflow and evil-magit co-existing, and it turns
    out that they just gave up and bumped whatever was on % for gitflow.
    Fair enough, I never used whatever was there anyways.

--- config/jws-vcs.el
+++ config/jws-vcs.el
@@ -7,13 +7,22 @@
     (diminish 'auto-revert-mode))
 
   (global-set-key (kbd "M-j g") 'magit-status)
-
   (after 'evil
-    (use-package evil-magit
-      :ensure t
-      :config
-      (require 'evil-magit))
-
     (define-key evil-normal-state-map (kbd "SPC g") 'magit-status)))
+
+(use-package magit-gitflow
+  :ensure t
+  :after magit
+  :config
+  (require 'magit-gitflow)
+  (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+  (define-key magit-mode-map "%" 'magit-gitflow-popup))
+
+(use-package evil-magit
+  :ensure t
+  :after (evil magit)
+  :config
+  (require 'evil-magit)
+  (define-key magit-mode-map (kbd "C-f") nil))
 
 (provide 'jws-vcs)
