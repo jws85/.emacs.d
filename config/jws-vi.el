35920c3c17bf14b77b2d67b070214c6655766f70
J. W. Smith <jwsmith2spam at gmail dot com>
2018-08-14 12:20:22 -0400

    use-package :config does not need progn
    Also, moved (evil-mode t) into :config rather than :init
    
    My use-package code is being pulled, kicking and screaming, into the
    new style.

--- config/jws-vi.el
+++ config/jws-vi.el
@@ -20,46 +20,45 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (switch-to-buffer (other-buffer (current-buffer))))
 
 (use-package evil
-  :init (evil-mode t)
   :ensure t
-
   :config
-  (progn
-    ;; evil requires undo-tree
-    (after 'diminish
-      (diminish 'undo-tree-mode))
+  (evil-mode t)
 
-    ;; artist-mode is allergic to evil
-    (after 'evil
-      (add-hook 'artist-mode-hook 'evil-emacs-state))
+  ;; evil requires undo-tree
+  (after 'diminish
+    (diminish 'undo-tree-mode))
 
-    ;; Emulates surround.vim plugin (https://github.com/tpope/vim-surround)
-    (use-package evil-surround
-      :ensure t
-      :init (global-evil-surround-mode 1))
+  ;; artist-mode is allergic to evil
+  (after 'evil
+    (add-hook 'artist-mode-hook 'evil-emacs-state))
 
-    ;; Make escape work like in vim
-    (define-key evil-normal-state-map [escape] 'keyboard-quit)
-    (define-key evil-visual-state-map [escape] 'keyboard-quit)
-    (define-key minibuffer-local-map [escape] 'jws/minibuffer-keyboard-quit)
-    (define-key minibuffer-local-ns-map [escape] 'jws/minibuffer-keyboard-quit)
-    (define-key minibuffer-local-completion-map [escape] 'jws/minibuffer-keyboard-quit)
-    (define-key minibuffer-local-must-match-map [escape] 'jws/minibuffer-keyboard-quit)
-    (define-key minibuffer-local-isearch-map [escape] 'jws/minibuffer-keyboard-quit)
+  ;; Emulates surround.vim plugin (https://github.com/tpope/vim-surround)
+  (use-package evil-surround
+    :ensure t
+    :init (global-evil-surround-mode 1))
 
-    (define-key evil-normal-state-map (kbd "[ SPC") (bind (evil-insert-newline-above) (forward-line)))
-    (define-key evil-normal-state-map (kbd "] SPC") (bind (evil-insert-newline-below) (forward-line -1)))
-    (define-key evil-normal-state-map (kbd "[ e") (kbd "ddkP"))
-    (define-key evil-normal-state-map (kbd "] e") (kbd "ddp"))
-    (define-key evil-normal-state-map (kbd "[ b") 'previous-buffer)
-    (define-key evil-normal-state-map (kbd "] b") 'next-buffer)
-    (define-key evil-normal-state-map (kbd "[ q") 'previous-error)
-    (define-key evil-normal-state-map (kbd "] q") 'next-error)
+  ;; Make escape work like in vim
+  (define-key evil-normal-state-map [escape] 'keyboard-quit)
+  (define-key evil-visual-state-map [escape] 'keyboard-quit)
+  (define-key minibuffer-local-map [escape] 'jws/minibuffer-keyboard-quit)
+  (define-key minibuffer-local-ns-map [escape] 'jws/minibuffer-keyboard-quit)
+  (define-key minibuffer-local-completion-map [escape] 'jws/minibuffer-keyboard-quit)
+  (define-key minibuffer-local-must-match-map [escape] 'jws/minibuffer-keyboard-quit)
+  (define-key minibuffer-local-isearch-map [escape] 'jws/minibuffer-keyboard-quit)
 
-    (setq evil-default-cursor t
-	  lazy-highlight-cleanup nil
-	  lazy-highlight-max-at-a-time nil
-	  lazy-highlight-initial-delay 0)))
+  (define-key evil-normal-state-map (kbd "[ SPC") (bind (evil-insert-newline-above) (forward-line)))
+  (define-key evil-normal-state-map (kbd "] SPC") (bind (evil-insert-newline-below) (forward-line -1)))
+  (define-key evil-normal-state-map (kbd "[ e") (kbd "ddkP"))
+  (define-key evil-normal-state-map (kbd "] e") (kbd "ddp"))
+  (define-key evil-normal-state-map (kbd "[ b") 'previous-buffer)
+  (define-key evil-normal-state-map (kbd "] b") 'next-buffer)
+  (define-key evil-normal-state-map (kbd "[ q") 'previous-error)
+  (define-key evil-normal-state-map (kbd "] q") 'next-error)
+
+  (setq evil-default-cursor t
+        lazy-highlight-cleanup nil
+        lazy-highlight-max-at-a-time nil
+        lazy-highlight-initial-delay 0))
 
 (define-key jws/leader-map (kbd "<tab>") 'jws/switch-to-previous-buffer)
 (define-key jws/leader-map (kbd "k") 'kill-this-buffer)
