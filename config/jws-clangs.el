a2e9d6a04640801fbf398ecd7be685308f835997
J. W. Smith <jwsmith2spam at gmail dot com>
2018-10-23 15:49:31 -0400

    Added golang setup
    I'm probably going to play a bit with C++ and with these newfangled
    curly-brace languages as well.  Starting with Go, but I've been doing
    C++ at work and might try my hand at an application outside of work
    too.
    
    C++ is still cantankerous, but it's gotten better with C++11 and then
    subsequently as well.  Qt is really nice and has waaaaaayyyyy better
    documentation than GTK.
    
    Go feels like curly-brace, compilable Python.  It's not as scripty,
    but it stays out of your way about as well.
    
    I haven't played with Rust yet; if it supplants anything, it'd
    probably be C++.

--- config/jws-clangs.el
+++ config/jws-clangs.el
@@ -1,8 +1,14 @@
 ;; C-like languages
 
+;; Golang
+;; Useful: https://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/
+(use-package go-mode
+  :ensure t
+  :config
+  (add-hook 'before-save-hook 'gofmt-before-save))
+
 (add-hook 'c-mode-common-hook
           (lambda ()
-            (modify-syntax-entry ?_ "w")  ; underscore considered part of a "word"
-	    ))
+            (modify-syntax-entry ?_ "w")))  ; underscore considered part of a "word"
 
 (provide 'jws-clangs)
