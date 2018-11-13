59ab10db5876dc94985bfe9e318af4fc29f13348
J. W. Smith <jwsmith2spam at gmail dot com>
2018-11-13 14:42:03 -0500

    Defer loading of elpy
    q.v. https://emacs.stackexchange.com/questions/10065/how-can-i-defer-loading-elpy-using-use-package

--- config/jws-python.el
+++ config/jws-python.el
@@ -1,6 +1,10 @@
+(use-package python
+  :ensure t
+  :mode ("\\.py\\'" . python-mode))
+
 (use-package elpy
   :ensure t
-  :config
-  (elpy-enable))
+  :defer t
+  :init (with-eval-after-load 'python (elpy-enable)))
 
 (provide 'jws-python)
