505903409e1148cee18b653d6e68f69c488b93e1
J. W. Smith <jwsmith2spam at gmail dot com>
2018-11-13 22:06:43 -0500

    Set up golang mode

--- config/jws-clangs.el
+++ config/jws-clangs.el
@@ -2,11 +2,27 @@
 
 ;; Golang
 ;; Useful: https://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/
+(defun jws/golang-hook ()
+  ;; Format code on save (needs go get golang.org/x/tools/cmd/goimports)
+  (set gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save)
+
+  ;; Compile
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+
+  ;; Jump around code
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark))
+
 (use-package go-mode
   :ensure t
   :mode "\\.go\\'"
   :config
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (add-hook 'go-mode-hook 'jws/golang-hook))
+
+;; C and other similar languages
 
 (add-hook 'c-mode-common-hook
           (lambda ()
