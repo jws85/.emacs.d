64a18af4db0f95d64da93526272c1ade9ba5dc5e
J. W. Smith <jwsmith2spam at gmail dot com>
2018-11-17 18:15:44 -0500

    Restructured org-mode configuration
    It does mostly the same thing, but most of the config is now loaded
    from use-package, and I think it should also be deferred but org-mode
    is called early on anyways so I have no real way of knowing.
    
    Removed worf as I think it was designed for people not using evil --
    you have to be in insert mode for the keys to do anything -- and it by
    default annoyingly overrode the [] keys.

--- config/jws-org-mode.el
+++ config/jws-org-mode.el
@@ -32,12 +32,6 @@
 
 (require 'seq) ; required for seq-filter
 
-(use-package org
-  :ensure t
-  :init (require 'org-install))
-
-;; basic configuration ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (defvar jws/org-dir (expand-file-name "~/Org/")
   "The directory where `org-mode' files live.")
 
@@ -50,70 +44,20 @@
 (defvar jws/org-notes-dir (concat jws/org-dir "notes/")
   "The directory where `deft' notes files live.")
 
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-
-(after 'org
-  ;; Adding habits (recurring events)
-  (add-to-list 'org-modules 'org-habit))
-
-;; Render code inside code blocks
-(setq org-src-fontify-natively t)
+(use-package org
+  :commands (org-agenda org-capture org-export-dispatch org-store-link)
+  :ensure t
+  :mode (("\\.org\\'" . org-mode))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+  (add-to-list 'org-modules 'org-habit)
+  (setq org-src-fontify-natively t))
 
 ;; org-agenda ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package worf
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook 'worf-mode)
-  (define-key worf-mode-map (kbd "[") nil)
-  (define-key worf-mode-map (kbd "]") nil))
-
 (use-package org-alert
-  :ensure t)
-
-;; The following code only shows the first action to be done per heading
-;; This code is taken more or less verbatim from the emacs.cafe article above.
-(defun jws/org-current-is-todo ()
-  (string= "TODO" (org-get-todo-state)))
-
-(defun jws/org-agenda-skip-all-siblings-but-first ()
-  "Skip all but the first non-done entry."
-  (let (should-skip-entry)
-    (unless (jws/org-current-is-todo)
-      (setq should-skip-entry t))
-    (save-excursion
-      (while (and (not should-skip-entry) (org-goto-sibling t))
-        (when (jws/org-current-is-todo)
-          (setq should-skip-entry t))))
-    (when should-skip-entry
-      (or (outline-next-heading)
-          (goto-char (point-max))))))
-;; end snippet
-
-(setq org-log-done t
-      org-refile-targets '((nil . (:maxlevel . 9))
-                           (org-agenda-files . (:maxlevel . 9)))
-      org-refile-use-outline-path t
-      org-outline-path-complete-in-steps nil
-      org-archive-location "finished.org::datetree/*"
-      org-agenda-span 14
-      org-use-fast-todo-selection t
-      org-log-into-drawer t
-      org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
-      org-tag-alist '(("@personal" . ?p) ("@career" . ?c) ("@errand" . ?e) ("@travel" . ?t))
-      org-agenda-custom-commands
-      '(("p" "Personal" tags-todo "@personal"
-         ((org-agenda-overriding-header "Personal")
-          (org-agenda-skip-function #'jws/org-agenda-skip-all-siblings-but-first)))
-        ("c" "Career" tags-todo "@career"
-         ((org-agenda-overriding-header "Career")
-          (org-agenda-skip-function #'jws/org-agenda-skip-all-siblings-but-first)))
-        ("E" "Errands" tags-todo "@errand"
-         ((org-agenda-overriding-header "Errands")
-          (org-agenda-skip-function #'jws/org-agenda-skip-all-siblings-but-first)))
-        ("A" "Travel" tags-todo "@travel"
-         ((org-agenda-overriding-header "Travelling")
-          (org-agenda-skip-function #'jws/org-agenda-skip-all-siblings-but-first)))))
+  :ensure t
+  :after (org-agenda))
 
 (setq jws/agenda-files
       '("inbox.org" "gtd.org" "tickler.org" "recurring.org"))
@@ -130,45 +74,88 @@
   (message "jws/agenda-files: %S" jws/agenda-files)
   (message "org-agenda-files: %S" org-agenda-files))
 
-(if (and (file-exists-p jws/org-dir) (file-exists-p jws/org-agenda-dir))
-    (progn
-      (jws/load-org-settings))
-  (message "Please set up jws/org-dir and jws/org-agenda-dir to use org-agenda, etc."))
+(use-package org-agenda
+  :after (org)
+  :config
+  ;; The following code only shows the first action to be done per heading
+  ;; This code is taken more or less verbatim from the emacs.cafe article above.
+  (defun jws/org-current-is-todo ()
+    (string= "TODO" (org-get-todo-state)))
 
-(after 'org-agenda
-  ;; Enabling vim bindings in the agenda
+  (defun jws/org-agenda-skip-all-siblings-but-first ()
+    "Skip all but the first non-done entry."
+    (let (should-skip-entry)
+      (unless (jws/org-current-is-todo)
+        (setq should-skip-entry t))
+      (save-excursion
+        (while (and (not should-skip-entry) (org-goto-sibling t))
+          (when (jws/org-current-is-todo)
+            (setq should-skip-entry t))))
+      (when should-skip-entry
+        (or (outline-next-heading)
+            (goto-char (point-max))))))
+  ;; end snippet
+
+  ;; Only load org settings if org-dir & org-agenda-dir exist
+  ;; Either create these directories, or override the paths in site-lisp
+  (if (and (file-exists-p jws/org-dir) (file-exists-p jws/org-agenda-dir))
+      (jws/load-org-settings)
+    (message "Please set up jws/org-dir and jws/org-agenda-dir to use org-agenda, etc."))
+
+  (setq org-log-done t
+        org-refile-targets '((nil . (:maxlevel . 9))
+                             (org-agenda-files . (:maxlevel . 9)))
+        org-refile-use-outline-path t
+        org-outline-path-complete-in-steps nil
+        org-archive-location "finished.org::datetree/*"
+        org-agenda-span 14
+        org-use-fast-todo-selection t
+        org-log-into-drawer t
+        org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
+        org-tag-alist '(("@personal" . ?p) ("@career" . ?c) ("@errand" . ?e) ("@travel" . ?t)))
+
+  ;; Agenda vim key bindings
   (evil-add-hjkl-bindings org-agenda-mode-map 'emacs)
   (define-key org-agenda-mode-map (kbd "L") 'org-agenda-log-mode)
   (define-key org-agenda-mode-map (kbd "C") 'org-agenda-capture)
-  (define-key org-agenda-mode-map (kbd "d") 'org-agenda-goto-date))
+  (define-key org-agenda-mode-map (kbd "d") 'org-agenda-goto-date)
 
-;; Save after various edits in org-mode/org-agenda
-(advice-add 'org-deadline :after 'org-save-all-org-buffers)
-(advice-add 'org-refile :after 'org-save-all-org-buffers)
+  ;; Save after various edits in org-mode/org-agenda
+  (advice-add 'org-deadline :after 'org-save-all-org-buffers)
+  (advice-add 'org-refile :after 'org-save-all-org-buffers)
 
-(defun jws/show-org-agenda ()
-  "Run `org-agenda' and bring its buffer to the front.
-
-I like to `setq' the `initial-buffer-choice' to this function
-in my site-init.el.  This displays the `org-agenda' at startup."
-  (interactive)
-  (org-agenda-list)
-  (get-buffer "*Org Agenda*"))
+  ;; Define custom agenda views
+  (setq org-agenda-custom-commands
+        '(("p" "Personal" tags-todo "@personal"
+           ((org-agenda-overriding-header "Personal")
+            (org-agenda-skip-function #'jws/org-agenda-skip-all-siblings-but-first)))
+          ("c" "Career" tags-todo "@career"
+           ((org-agenda-overriding-header "Career")
+            (org-agenda-skip-function #'jws/org-agenda-skip-all-siblings-but-first)))
+          ("E" "Errands" tags-todo "@errand"
+           ((org-agenda-overriding-header "Errands")
+            (org-agenda-skip-function #'jws/org-agenda-skip-all-siblings-but-first)))
+          ("A" "Travel" tags-todo "@travel"
+           ((org-agenda-overriding-header "Travelling")
+            (org-agenda-skip-function #'jws/org-agenda-skip-all-siblings-but-first))))))
 
 ;; org-capture ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; http://orgmode.org/manual/Capture-templates.html
-(setq org-capture-templates
-      '(("t" "Todo" entry (file (lambda () (concat jws/org-agenda-dir "inbox.org")))
-         "* TODO %?\n  %i")
-        ("T" "Tickler" entry (file (lambda () (concat jws/org-agenda-dir "tickler.org")))
-         "* TODO %i%?\n  %T")
-        ("l" "Link" entry (file+headline (lambda () (concat jws/org-dir "links.org")) "Uncategorized")
-         "* %?")
-        ("j" "Journal" entry (file+datetree (lambda () (concat jws/org-journal-dir (format-time-string "%Y") ".org")))
-         "* %?\n  Entered on %U\n  %i")
-        ("s" "Shopping list" entry (file+headline (lambda () (concat jws/org-dir "shopping.org")) "Unfiled Shopping")
-         "* %?\n  Entered on %U\n  %i")))
+(use-package org-capture
+  :after (org)
+  :config
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file (lambda () (concat jws/org-agenda-dir "inbox.org")))
+           "* TODO %?\n  %i")
+          ("T" "Tickler" entry (file (lambda () (concat jws/org-agenda-dir "tickler.org")))
+           "* TODO %i%?\n  %T")
+          ("l" "Link" entry (file+headline (lambda () (concat jws/org-dir "links.org")) "Uncategorized")
+           "* %?")
+          ("j" "Journal" entry (file+datetree (lambda () (concat jws/org-journal-dir (format-time-string "%Y") ".org")))
+           "* %?\n  Entered on %U\n  %i")
+          ("s" "Shopping list" entry (file+headline (lambda () (concat jws/org-dir "shopping.org")) "Unfiled Shopping")
+           "* %?\n  Entered on %U\n  %i"))))
 
 ;; note-taking (deft) ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
