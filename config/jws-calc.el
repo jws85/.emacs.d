741b979af0bf97b6c9a686db32977a5344d3703d
J. W. Smith <jwsmith2spam at gmail dot com>
2019-07-10 13:41:33 -0400

    Define favorite units
    To have the currency work properly, you'll need calc-currency

--- config/jws-calc.el
+++ config/jws-calc.el
@@ -25,6 +25,19 @@
       calc-currency-update-interval 3)
 (setq calc-start-hook #'calc-currency-load)
 
+;; Favorite units are all currency:
+;;  u 1: US dollar
+;;  u 2: Canadian dollar
+;;  u 3: Australian dollar
+;;  u 4: Mexican peso
+;;  u 5: Euro
+;;  u 6: Japanese yen
+;;  u 7: Chinese yuan/renminbi
+;;  u 8: Hong Kong dollar
+;;  u 9: Singaporean dollar
+;;  u 0: South Korean won
+(setq var-Units (calc-eval "[USD, CAD, AUD, MXN, EUR, JPY, CNY, HKD, SGD, KRW]" 'raw))
+
 (provide 'jws-calc)
 
 ;;; jws-calc.el ends here
