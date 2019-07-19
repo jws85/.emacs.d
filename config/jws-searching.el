528d00b66321b9e6107faaa504d63e2f0bb6726d
J. W. Smith <jwsmith2spam at gmail dot com>
2019-07-19 12:37:06 -0400

    Open recently opened file
    I'm amazed I didn't set this up earlier!!!!!

--- config/jws-searching.el
+++ config/jws-searching.el
@@ -77,6 +77,7 @@ directories will have a trailing /, ignore it so foo.txt is after foo/."
 
     (define-key jws/leader-map (kbd "SPC") 'counsel-M-x)
     (define-key jws/leader-map (kbd "f f") 'counsel-find-file)
+    (define-key jws/leader-map (kbd "f r") 'counsel-recentf)
     (define-key jws/leader-map (kbd "f b") 'counsel-bookmark)
     (define-key jws/leader-map (kbd "b b") 'ivy-switch-buffer)))
 
