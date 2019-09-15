b0f6f13a734445a0ab23a1872e79912abe74927b
J. W. Smith <jwsmith2spam at gmail dot com>
2019-09-14 23:49:16 -0400

    Get rid of the customize junk once and for all
    (I hope???)

--- init.el
+++ init.el
@@ -4,17 +4,3 @@
 
 ;; Untangle my config from its org-mode file and load it
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ivy-rich nyan-mode minions doom-modeline use-package smex paradox focus flx evil-surround evil-collection doom-themes counsel))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
